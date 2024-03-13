5868d671f179b46976cf757eec682234194d329a
Confrontend <hamed.fatehi.66@gmail.com>
2024-03-13 16:21:01 +0100

    using nextjs font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
